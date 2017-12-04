eafc5689ae745bfcd7613f4e79a3ae5d4470b0dc
Nikhil.Agrawal <nikhil.agrawal@target.com>
2017-12-04 14:50:43 +0530

    adding basic classes

---
+++ src/main/java/org/sample/spring/controller/NoteController.java
@@ -0,0 +1,71 @@
+package org.sample.spring.controller;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import org.sample.spring.model.Note;
+import org.sample.spring.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@Api(value="NoteController", description="Operations pertaining to notes database entries")
+public class NoteController {
+
+    @Autowired
+    NoteRepository noteRepository;
+
+    @GetMapping("/notes")
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    @GetMapping("/notes/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable(value = "id") Long noteId) {
+        Note note = noteRepository.findOne(noteId);
+        if(note == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(note);
+    }
+
+	@RequestMapping(value = "/notes",
+			method = RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "API to post Notes data",
+			notes = "API to post Notes data",
+			response = Note.class, responseContainer = "Note")
+    public Note createNote(@Valid @RequestBody Note note) {
+        return noteRepository.save(note);
+    }
+
+    @PutMapping("/notes/{id}")
+    public ResponseEntity<Note> updateNote(@PathVariable(value = "id") Long noteId,
+										   @Valid @RequestBody Note noteDetails) {
+        Note note = noteRepository.findOne(noteId);
+        if(note == null) {
+            return ResponseEntity.notFound().build();
+        }
+        note.setTitle(noteDetails.getTitle());
+        note.setContent(noteDetails.getContent());
+
+        Note updatedNote = noteRepository.save(note);
+        return ResponseEntity.ok(updatedNote);
+    }
+
+    @DeleteMapping("/notes/{id}")
+    public ResponseEntity<Note> deleteNote(@PathVariable(value = "id") Long noteId) {
+        Note note = noteRepository.findOne(noteId);
+        if(note == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        noteRepository.delete(note);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/org/sample/spring/repository/NoteRepository.java
@@ -0,0 +1,9 @@
+package org.sample.spring.repository;
+
+import org.sample.spring.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
